ab374c1b24ec497a2cc91f8ba5ce74ab7e66e14e
Ashketchup143 <anmlim@addu.edu.ph>
2023-10-10 19:29:51 +0800

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,55 @@
+import { useEffect, useState } from "react"
 import Button from "./components/Button"
 import Post from "./components/Post"
+import useFetch from "./hooks/useFetch"
 
 
 function App() {
+  const[number, setNumber]=useState(1)
+  // const[post, setPost]=useState("")
+  // const fetchPost = async(id) => {
+  //   try{
+  //     const response = await fetch('https://jsonplaceholder.typicode.com/posts/' + id)
+  //     const data= await response.json()
 
-  console.log(Array.from({length:5}))
+  //     setPost(data)
+  //   } catch(error){
+  //     console.log(error)
+  //   } finally{
+  //     console.log('Done')
+  //   }
+
+  // }
+
+  const {data}=useFetch('https://jsonplaceholder.typicode.com/posts/', number)
+
+  // useEffect(()=>{
+    // fetch('https://jsonplaceholder.typicode.com/posts/' + number)
+    // .then((response) => response.json())
+    // .then((data)=>setPost(data))
+    // .catch((error)=> console.log(error))
+    // fetchPost(number)
+  // }, [number])
+
+  useEffect(()=>{
+    console.log("Mounted")
+    return()=>{
+      console.log('Remove')
+    }
+  },[])
+
+  
+  
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center">
-      <Post/>
+      {data && <Post title={data.title} body={data.body}/>}
+      {number}
       <div className="flex items-center gap-3 mt-3">
       {Array.from({length:5}).map((value, index) => {
         return(
-          <div className="p-4 border font-bold hover:cursor-pointer">
+          <div key={`pagination-${index+1}`} className="p-4 border font-bold hover:cursor-pointer" onClick={()=> setNumber(index+1)}>
             {index+1}
+            
           </div>
         )
       })}
